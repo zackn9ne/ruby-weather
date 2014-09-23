9e700f95dd5e0f5648dc896b8cd60d442f7cd713
zackn9ne <zackn9ne@gmail.com>
2014-09-23 18:48:37 -0400

    running works* and leangit add -A .

--- ruby-weather.rb
+++ ruby-weather.rb
@@ -15,9 +15,10 @@ class UpdateBklyn
         if @key == ''
             puts "error no file found in ~/.rubyweather/config \n consult github.com/zackn9ne to figure out what happened"
             exit
-        elsif
-            puts "MASTER_KEY FOUND! Access granted! \n
-            using #{@@key}"
+else
+        #elsif
+            #puts "MASTER_KEY FOUND! Access granted! \n
+            #using #{@@key}"
         end
     end
 
@@ -25,22 +26,17 @@ class UpdateBklyn
         cities = { :BK => "http://api.wunderground.com/api/#{@@key}/geolookup/conditions/q/NY/brooklyn.json", :NY => "http://api.wunderground.com/api/#{@@key}/geolookup/conditions/q/NY/manhattan.json" }
         #save file
         url = cities[:BK] 
-        link_data = `wget #{url} -O #{@@app_path}brooklyn.json`
+        link_data = `wget -q #{url} -O #{@@app_path}brooklyn.json`
         link_data
     end
 
     def read_json_file
-        puts "reading json..."
         jsonfile = [@@app_path, "brooklyn.json"].join
-puts jsonfile
         @file_contents = File.open(   jsonfile, 'r') #=> brooklyn.json
-        puts "opening #{jsonfile}"
         @json_chunk = JSON.load(@file_contents)  
-        puts @file_contents
     end
 
     def parse_json_file
-        puts "parsing json..."
         @temp = @json_chunk['current_observation']['temp_f']
         @wind = @json_chunk['current_observation']['wind_mph']
         @wind_gust = @json_chunk['current_observation']['wind_gust_mph']
@@ -53,19 +49,12 @@ puts jsonfile
         puts @info
     end
 
-    def create_and_write_md_file
-        prompt_file = [@@app_path,"temp.md"].join
-        file_contents = File.open(prompt_file, 'w')
-        file_contents.write(@info)
-        puts "temp saved to file: #{prompt_file}"
-        file_contents.close
-    end
 end
 
 
 
 def forced_update
-    puts "forcing update..."
+    puts "downloading..."
     ub = UpdateBklyn.new
     ub.key_check
     ub.download
@@ -88,24 +77,15 @@ end
 def auto_update
     #read file
     modified = File.mtime("#{@@app_path}brooklyn.json")
-puts "checking #{@@app_path}brooklyn.json"
     current_time = Time.now
-    #puts modified
-    #puts current_time
     if current_time - modified > 18
-        puts "your files too old i am making you referesh it"
+        #puts "your files too old i am making you referesh it"
         forced_update
     elsif
         spit_cached_data
     end
 end
 auto_update
-#off
-def reload_bash_mac
-    reload = `source ~/.bash_profile`
-    reload
-    puts  "reloading bash"
-end
 
 
 def specific_json_values 
